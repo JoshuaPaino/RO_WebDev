e2f7682e8a675b2ca975b2d87caeb2d8503b81a3
JoshuaPaino <joshua.paino@gmail.com>
2022-10-24 12:39:34 -0500

    Finish project

--- Project5_BlockManipulation/script.js
+++ Project5_BlockManipulation/script.js
@@ -16,10 +16,48 @@ window.addEventListener('load', function () {
     size.addEventListener('input', function () {
         block.style.transform = `scale(${size.value})`;
     })
-    // Opacity manip
+    // opacity manip
     let opacity = this.document.getElementById('opacity');
     opacity.addEventListener('input', function () {
         block.style.opacity = opacity.value;
     })
-    // SHape
+    // shape manip
+    let selector = this.document.getElementById('shape-select');
+    let okButton = this.document.getElementById('ok-shape');
+    okButton.addEventListener('click', function () {
+        let option = selector.value;
+        if (option === '1') {
+            block.style.borderRadius = '0';
+            rotateDeg = 0;
+            block.style.transform = `scale(${scaleCoef}) rotate(${rotateDeg}deg)`;;
+        } else if (option === '2') {
+            block.style.borderRadius = '50%';
+        } else if (option === '3') {
+            block.style.borderRadius = '0';
+            rotateDeg = 45;
+            block.style.transform = `scale(${scaleCoef}) rotate(${rotateDeg}deg)`;;
+        }
+    })
+
+    // Hex manip
+    let hex = this.document.getElementById('hex');
+    hex.addEventListener('keyup', function (e) {
+        if (e.key === 'Enter') {
+            block.style.backgroundColor = `#${hex.value}`;
+        }
+    })
+
+    // RGBA manip
+    let rgbaR = this.document.getElementById('rgba-r');
+    let rgbaG = this.document.getElementById('rgba-g');
+    let rgbaB = this.document.getElementById('rgba-b');
+    let rgbaA = this.document.getElementById('rgba-a');
+
+    let rgbaInputs = this.document.querySelectorAll('.rgba-container input');
+
+    rgbaInputs.forEach(function (element) {
+        element.addEventListener('input', function () {
+            block.style.backgroundColor = `rgba(${rgbaR.value}, ${rgbaG.value}, ${rgbaB.value}, ${rgbaA.value})`;
+        })
+    })
 })
